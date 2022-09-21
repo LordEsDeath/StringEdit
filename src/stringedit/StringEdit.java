ddf5d366bd08016028751a589536cda675653e29
pupil <pupil@ivkhk.ee>
2022-09-21 11:15:36 +0300

    Редакт. кода

--- src/stringedit/StringEdit.java
+++ src/stringedit/StringEdit.java
@@ -5,6 +5,8 @@
  */
 package stringedit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
@@ -15,6 +17,38 @@ public class StringEdit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("==== StringEdit ====");
+        System.out.println("=====================");
+        System.out.println("Введите текст (Латиницей): ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        do{
+            System.out.print("Выберите номер функции: \n");
+            System.out.println("0. Вывод из программы");
+            System.out.println("1. Вывести количество символов");
+            System.out.println("2. Вывести количество слов");
+            System.out.println("3. Найти слово в строке");
+            System.out.println("4. Заменить указаное слово в строке");
+            int number = scanner.nextInt();
+            if(number == 0){
+                break;
+            }else if(number == 1){
+                System.out.print("Вы выбрали номер: " + number + "\n");
+                System.out.println("Количество символов:" +str.length());
+            }else if(number == 2){
+                System.out.print("Вы выбрали номер: " + number + "\n");
+                String[] words = str.split(" ");
+                System.out.println("Количество слов: "+ words.length);
+            }else if(number == 3){
+                System.out.print("Вы выбрали номер: " + number + "\n");
+                break;
+                
+            }else if(number == 4){
+                System.out.print("Вы выбрали номер: " + number + "\n");
+                break;
+                
+            }
+        }while(true);
         // TODO code application logic here
     }
     
